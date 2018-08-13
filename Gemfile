cd6aca2e3b877c46305efe2c94b1c098c88100e7
Yada <ydt.hran2@gmail.com>
2018-08-13 22:43:09 +0900

    UserStream 廃止対応
    タイムラインの取得は15回/15分
    リストの取得は900回/15分
    フォローしてる人全員を入れたリストを作ってそれを1秒毎に取得する

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
 source 'https://rubygems.org'
 
-gem 'twitter', '~>5.14.0'
+gem 'twitter', '~>6.2.0'
--- bot.rb
+++ bot.rb
@@ -2,38 +2,52 @@
 
 require "twitter"
 
+
 CONSUMER_KEY          = "your consumer key"
 CONSUMER_SECRET       = "your consumer secret"
 ACCESS_TOKEN          = "your access token"
 ACCESS_TOKEN_SECRET   = "your access token secret"
 
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = CONSUMER_KEY
-  config.consumer_secret     = CONSUMER_SECRET
-  config.access_token        = ACCESS_TOKEN
+# フォローしてる人がすべて入ってるリスト（5000人超えると使えなくなるので注意）
+LIST_ID = 1028961406517829632
+
+
+$client = Twitter::REST::Client.new do |config|
+  config.consumer_key = CONSUMER_KEY
+  config.consumer_secret = CONSUMER_SECRET
+  config.access_token = ACCESS_TOKEN
   config.access_token_secret = ACCESS_TOKEN_SECRET
 end
+$my_screen_name = $client.user.screen_name
 
-stream = Twitter::Streaming::Client.new do |config|
-  config.consumer_key        = CONSUMER_KEY
-  config.consumer_secret     = CONSUMER_SECRET
-  config.access_token        = ACCESS_TOKEN
-  config.access_token_secret = ACCESS_TOKEN_SECRET
-end
-
-my_screen_name = client.user.screen_name
-stream.user do |tweet|
-  # DMとか入ってくるので
-  next unless tweet.is_a?(Twitter::Tweet)
+def thon_if_required(tweet)
+  # DMとかは除外
+  return unless tweet.is_a?(Twitter::Tweet)
   # RTは除外
-  next unless tweet.retweeted_status.nil?
+  return unless tweet.retweeted_status.nil?
   # 無限ループになるので自分のツイートには何もしない
-  next if tweet.user.screen_name == my_screen_name
+  return if tweet.user.screen_name == $my_screen_name
 
   if tweet.text.include?("てょーん")
-    client.favorite(tweet)
+    $client.favorite(tweet)
     status = "@#{tweet.user.screen_name} ٩( ๑╹ ꇴ╹)۶てょーん"
-    client.update(status, in_reply_to_status: tweet)
+    $client.update(status, in_reply_to_status: tweet)
   end
 end
 
+last_id = nil
+loop do
+  sleep(1)
+
+  if last_id.nil?
+    last_id = $client.list_timeline(LIST_ID, count: 1, include_rts: false).first.id
+    next
+  end
+  list_tweets = $client.list_timeline(LIST_ID, count: 200, include_rts: false, since_id: last_id)
+  next if list_tweets.empty?
+
+  list_tweets.each do |tw|
+    thon_if_required(tw)
+  end
+  last_id = list_tweets.map(&:id).max
+end
